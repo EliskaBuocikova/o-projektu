775f0b8581d6b5e53234ca6b7d156e1a8c4349e4
Eliška Buociková <buocikovaeliska@gmail.com>
2023-05-28 23:07:47 +0200

    drobné úpravy v pozicování prvnků

--- src/ErrorPage/index.jsx
+++ src/ErrorPage/index.jsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
-import Main from "../Main/Main";
-
+import Main from '../Main/Main';
 
 const ErrorPage = () => {
-    return (
-        <>
-            <Header />
-                <Main 
-                text={"Nenapsal/a jsi do URL nějaký nesmysl? Zkus to ještě jednou..."}    
-                />
-            <Footer text= "Czechitas, Digitální akademie: Web - jaro 2023"/>
-        </>
-    )
-    };
+  return (
+    <>
+      <Header />
+      <Main
+        text={'Nenapsal/a jsi do URL nějaký nesmysl? Zkus to ještě jednou...'}
+      />
+      <Footer text="Czechitas, Digitální akademie: Web - jaro 2023" />
+    </>
+  );
+};
 
 export default ErrorPage;
